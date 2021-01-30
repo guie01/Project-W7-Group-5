bec6eaa9aa7bf3941eb3f980e640294a977dd21a
Guillermo Garcia <ggq0107@hotmail.com>
2021-01-30 11:23:19 -0500

    initial functionality nps api

--- assets/trails.js
+++ assets/trails.js
@@ -1,6 +1,6 @@
-var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + state + "&api_key=vLYmRUPhuQGHTx4RzNCKvg3aovxdPVYFBBIIjKBJ"
+var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=vLYmRUPhuQGHTx4RzNCKvg3aovxdPVYFBBIIjKBJ"
 
-var state = $("#searchPlace").val().trim();
+var state = $("#searchPlace").val();
 
     $.ajax({
         url: queryURL,
@@ -12,29 +12,29 @@ var state = $("#searchPlace").val().trim();
 
          console.log(response);
 
-        var parkInfoCont = $("<div>");
-        var parkAddress = $("<p>");
-        var parkHours = $("<p>");
-        var parkDescription = $("<p>");
-        var parkActivies = $("<p>");
+        var parkInfoCont = $("#adventure-results");
+        var parkAddress = $("#address");
+        var parkHours = $("#parkHours");
+        var parkDescription = $("#description");
+        var parkActivies = $("#activities");
 
         // Park Address 
         parkInfoCont.append(parkAddress);
         parkAddress.text(JSON.stringify(response.data[0].addresses[0]))
-        $("#container").append(parkInfoCont);
+        $("#adventure-results").append(parkInfoCont);
         console.log(parkAddress);
 
         //Park Hours
         parkHours.text(JSON.stringify(response.data[0].operatingHours[0].standardHours));
-        $("#container").append(parkHours);
+        $("#adventure-results").append(parkHours);
 
         //Park Description
         parkDescription.text(response.data[0].description);
-        $("#container").append(parkDescription);
+        $("#adventure-results").append(parkDescription);
 
         //Park Activities
         parkActivies.text(response.data[0].activities[0].name);
-        $("#container").append(parkActivies);
+        $("#adventure-results").append(parkActivies);
         
 
         var latLong = response.data[0].latLong;
